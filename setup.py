770f5b01759a6c463e53adb1bae237b045334395
Miguel Alex Cantu <miguel.cantu@rackspace.com>
2016-01-18 14:42:40 +0000

    added license and updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='parse_apache_configs',
       version="0.1",
       description="A python module to parse apache config files.",
-      url='http://github.com/elextro/parse_apache_configs',
+      url='http://github.com/alextricity25/parse_apache_configs',
+      download_url = 'https://github.com/alextricity25/parse_apache_configs/tarball/0.1',
       author='Miguel Alex Cantu',
-      author_email='miguel.cantu@rackspace.com',
-      license='MIT',
+      author_email='miguel.cantu@rackspace.com'
       packages=['parse_apache_configs'],
       zip_safe=False)
