d2bcf358a560aeed56e61e903d97f4125ded30e1
Miguel Alex Cantu <miguel.cantu@rackspace.com>
2016-01-19 19:42:42 +0000

    Updated install_requires

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(name='parse_apache_configs',
       url='http://github.com/alextricity25/parse_apache_configs',
       download_url = 'https://github.com/alextricity25/parse_apache_configs/tarball/0.1',
       author='Miguel Alex Cantu',
-      author_email='miguel.cantu@rackspace.com'
+      author_email='miguel.cantu@rackspace.com',
       packages=['parse_apache_configs'],
+      install_requires=[
+          'pyparsing',
+      ],
       zip_safe=False)
