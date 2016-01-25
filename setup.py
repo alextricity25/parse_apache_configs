a031b7c7dc535f25e99df8a5605c12277293ad26
elextro <miguel.cantu@rackspace.com>
2016-01-25 12:10:08 -0600

    Changed versioning

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='parse_apache_configs',
-      version="0.1",
+      version="0.0.1",
       description="A python module to parse apache config files.",
       url='http://github.com/alextricity25/parse_apache_configs',
-      download_url = 'https://github.com/alextricity25/parse_apache_configs/tarball/0.1',
+      download_url = 'https://github.com/alextricity25/parse_apache_configs/tarball/0.0.1',
       author='Miguel Alex Cantu',
       author_email='miguel.cantu@rackspace.com',
       packages=['parse_apache_configs'],
